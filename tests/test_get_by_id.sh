651614824d07e09b2d7f98a38cd48ff4d24cb717
Julien Rouzieres <julien.rouzieres@mac.com>
2020-05-09 16:12:32 +0200

    Better tests

--- tests/test_get_by_id.sh
+++ tests/test_get_by_id.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
+set -ex
 
 echo '{"foo":[{"id":1,"bar":"baz"}]}' | $LJS &
-
 PID=$!
 
 RES=`curl -s http://localhost:8080/foo/1`
 
 kill -s SIGINT $PID
-
-echo "$RES"
 
 [ "$RES" == '{"id":1,"bar":"baz"}' ]
--- tests/test_not_found.sh
+++ tests/test_not_found.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
+set -ex
 
 echo '{"foo":[{"id":1,"bar":"baz"}]}' | $LJS &
-
 PID=$!
 
 RES=`curl -s http://localhost:8080/bar`
 
 kill -s SIGINT $PID
-
-echo "$RES"
 
 [ "$RES" == '{"msg":"Not found"}' ]
